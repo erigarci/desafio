830bda395d4cafb71975d070b9fd991f27a82cf3
erigarci <erigarci29@gmail.com>
2023-07-19 21:22:26 -0300

    Estructura Inicial

---
+++ desafio/settings/local.py
@@ -0,0 +1,23 @@
+from .base import *
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-w3&jyuc62%oy904mkc3v$d9sq0u3(q)zt=!gu5nnqqbx0s$-cc'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
